c6b529e5aa1ca9ca15ca86aeda804836606c6817
udit-maurya1 <154532269+udit-maurya1@users.noreply.github.com>
2024-11-24 21:44:53 +0530

    maximum subarray sum

---
+++ kadane's algo.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        int currsum=0 , maxsum=INT_MIN;
+        int n =arr.size();
+        for(int i =0;i<n;i++){
+            currsum+=arr[i];
+            maxsum=max(currsum,maxsum);
+            if(currsum<=0)
+            currsum=0;
+        }
+        return maxsum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.maxSubarraySum(arr) << endl;
+    }
+}
